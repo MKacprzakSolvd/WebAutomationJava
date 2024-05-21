6c6bc459e3fb1242fb21cb283283d4c6e71a5661
Mateusz Kacprzak <170347259+MKacprzakSolvd@users.noreply.github.com>
2024-05-21 14:31:51 +0200

    initial commit - opening page in chrome

---
+++ src/main/java/com/solvd/App.java
@@ -0,0 +1,22 @@
+package com.solvd;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class App {
+    public static void main(String[] args) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        driver.get("https://magento.softwaretestingboard.com/");
+        driver.quit();
+    }
+}
