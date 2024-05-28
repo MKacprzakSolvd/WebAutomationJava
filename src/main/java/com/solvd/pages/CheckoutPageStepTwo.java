b86d8c49497ef0a01e0a054c83589708b8efd943
Mateusz Kacprzak <170347259+MKacprzakSolvd@users.noreply.github.com>
2024-05-28 11:31:43 +0200

    fixed waiting for button to load in checkout step two

--- src/main/java/com/solvd/pages/CheckoutPageStepTwo.java
+++ src/main/java/com/solvd/pages/CheckoutPageStepTwo.java
@@ -1,5 +1,6 @@
 package com.solvd.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,22 +16,24 @@ public class CheckoutPageStepTwo {
     @FindBy(css = "#checkout-payment-method-load [type='submit']")
     private WebElement placeOrderButton;
 
+    //.checkout-billing-address
     @FindBy(css = "#checkout-payment-method-load .payment-method-content")
     private WebElement orderAddressWrapper;
 
     public CheckoutPageStepTwo(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+        waitTillPageLoads();
     }
 
     public CheckoutPageStepThree placeOrder() {
-        waitTillPageLoads();
         this.placeOrderButton.click();
         return new CheckoutPageStepThree(this.driver);
     }
 
     private void waitTillPageLoads() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("body > div.loading-mask")));
         wait.until(ExpectedConditions.elementToBeClickable(this.placeOrderButton));
         wait.until(ExpectedConditions.visibilityOf(this.orderAddressWrapper));
     }
