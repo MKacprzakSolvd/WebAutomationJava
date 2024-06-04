fe4015ab6eb5ee40634bddf8258718d3ebe11474
Mateusz Kacprzak <170347259+MKacprzakSolvd@users.noreply.github.com>
2024-06-04 12:28:35 +0200

    rough webdriver switching implementation

--- src/main/java/com/solvd/util/AbstractTest.java
+++ src/main/java/com/solvd/util/AbstractTest.java
@@ -7,24 +7,56 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Properties;
 
 public abstract class AbstractTest {
     private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     @BeforeMethod
     public void setUp() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        // TODO: REFACTOR INTO CONFIG CLASS!
+        String webPropertiesPath = Thread.currentThread().getContextClassLoader().getResource("web.properties").getPath();
+        Properties webProperties = new Properties();
         try {
-            this.driver.set(
-                    new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions));
-            //this.driver.set(
-            //        new RemoteWebDriver(new URL("http://localhost:4444"), firefoxOptions));
-        } catch (MalformedURLException e) {
+            webProperties.load(new FileInputStream(webPropertiesPath));
+        } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        String webdriver = "";
+        if (System.getProperties().containsKey("webdriver")) {
+            webdriver = System.getProperty("webdriver");
+        } else if (webProperties.containsKey("webdriver")) {
+            webdriver = webProperties.getProperty("webdriver");
+        }
+
+        String seleniumUrlText = "http://localhost:4444";
+        URL seleniumUrl = null;
+        try {
+            seleniumUrl = new URL(seleniumUrlText);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(
+                    "Malformed selenium url (%s)".formatted(seleniumUrlText), e);
+        }
+
+        // TODO: add enum for webdriver
+        switch (webdriver) {
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                this.driver.set(
+                        new RemoteWebDriver(seleniumUrl, chromeOptions));
+                break;
+            case "gecko":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                this.driver.set(
+                        new RemoteWebDriver(seleniumUrl, firefoxOptions));
+                break;
+            default:
+                throw new IllegalStateException("Unexpected webdriver: '" + webdriver + "'");
         }
 
         driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
